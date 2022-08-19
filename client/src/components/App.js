999e35ffa5c68235fbfcd48655a79f6bc4a913a0
Leigh Ryffel <leightryffel@gmail.com>
2022-08-19 13:19:37 -0400

    created files for react components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
@@ -30,18 +28,9 @@ function App() {
         <Switch>
           <Route path="/testing">
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
               <p>
                 Edit <code>src/App.js</code> and save to reload.
               </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
             </header>
           </Route>
           <Route path="/">
